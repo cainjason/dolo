81338da6547b88dbaafb241c062a9e2d70e78aae
Jason Cain <118203097+cainjason@users.noreply.github.com>
2023-12-06 16:11:57 -0500

    Chore: created components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Body from './components/Body';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="app">
       Dolo
+      <Header />
+      <Navbar />
+      <Body />
+      <Footer />
     </div>
   );
 }
