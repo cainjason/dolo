70077962b361ded5ed1ca38dbef1c037ed3f9ac4
Jason Cain <118203097+cainjason@users.noreply.github.com>
2023-12-08 08:07:23 -0500

    Chore: setup header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
 function Header() {
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <h2>Header</h2>
+      <input type="search" placeholder="G.R.A.N.T. Church A.M.E."></input>
+    </Container>
   )
 }
 
-export default Header
+export default Header;
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    background-color: blueviolet;
+`;
